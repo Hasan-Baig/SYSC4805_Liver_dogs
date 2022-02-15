a917b2014b36964de23e8bc0d56304f448b3dadc
stephenwicklund@cmail.carleton.ca <stephenwicklund@cmail.carleton.ca>
2022-02-15 16:13:05 -0500

    Lock the plow position after moving

--- python/plow.py
+++ python/plow.py
@@ -10,7 +10,13 @@ class Plow():
     def openPlow(self):
         sim.simxSetJointTargetVelocity(self.clientID,self.leftPlowJoint,0.2,sim.simx_opmode_blocking)
         sim.simxSetJointTargetVelocity(self.clientID,self.rightPlowJoint,0.2,sim.simx_opmode_blocking)
+        time.sleep(1)
+        sim.simxSetJointTargetVelocity(self.clientID,self.leftPlowJoint,0,sim.simx_opmode_blocking)
+        sim.simxSetJointTargetVelocity(self.clientID,self.rightPlowJoint,0,sim.simx_opmode_blocking)
     
     def closePlow(self):
         sim.simxSetJointTargetVelocity(self.clientID,self.leftPlowJoint,-0.2,sim.simx_opmode_blocking)
         sim.simxSetJointTargetVelocity(self.clientID,self.rightPlowJoint,-0.2,sim.simx_opmode_blocking)
+        time.sleep(1)
+        sim.simxSetJointTargetVelocity(self.clientID,self.leftPlowJoint,0,sim.simx_opmode_blocking)
+        sim.simxSetJointTargetVelocity(self.clientID,self.rightPlowJoint,0,sim.simx_opmode_blocking)
