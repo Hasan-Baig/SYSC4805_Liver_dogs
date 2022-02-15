ae02330eaff33325a8d87c1a2f87f86f2bec62c3
stephenwicklund@cmail.carleton.ca <stephenwicklund@cmail.carleton.ca>
2022-02-15 15:22:20 -0500

    Added plow.py to control the plow.

---
+++ python/plow.py
@@ -0,0 +1,16 @@
+import sim
+import time
+
+class Plow():
+    def __init__(self,clientID):
+        self.clientID = clientID
+        res,self.leftPlowJoint = sim.simxGetObjectHandle(clientID,'LeftPlowJoint',sim.simx_opmode_blocking)
+        res,self.rightPlowJoint = sim.simxGetObjectHandle(clientID,'RightPlowJoint',sim.simx_opmode_blocking)
+
+    def openPlow(self):
+        sim.simxSetJointTargetVelocity(self.clientID,self.leftPlowJoint,0.2,sim.simx_opmode_blocking)
+        sim.simxSetJointTargetVelocity(self.clientID,self.rightPlowJoint,0.2,sim.simx_opmode_blocking)
+    
+    def closePlow(self):
+        sim.simxSetJointTargetVelocity(self.clientID,self.leftPlowJoint,-0.2,sim.simx_opmode_blocking)
+        sim.simxSetJointTargetVelocity(self.clientID,self.rightPlowJoint,-0.2,sim.simx_opmode_blocking)
